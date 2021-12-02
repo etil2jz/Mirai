2b84ab080312d69290f168a83943fd8b262015b5
Etil <81570777+etil2jz@users.noreply.github.com>
2021-12-02 15:00:12 +0100

    [ci skip] build gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,17 @@
+import io.papermc.paperweight.util.constants.*
+
 plugins {
     java
+    `maven-publish`
     id("com.github.johnrengelman.shadow") version "7.1.0" apply false
     id("io.papermc.paperweight.patcher") version "1.3.1"
 }
 
 repositories {
     mavenCentral()
-    maven("https://papermc.io/repo/repository/maven-public/")
+    maven("https://papermc.io/repo/repository/maven-public/") {
+        content { onlyForConfigurations(PAPERCLIP_CONFIG) }
+    }
 }
 
 dependencies {
@@ -15,14 +20,27 @@ dependencies {
     paperclip("io.papermc:paperclip:3.0.2")
 }
 
-subprojects {
+allprojects {
     apply(plugin = "java")
+    apply(plugin = "maven-publish")
 
-    java { toolchain { languageVersion.set(JavaLanguageVersion.of(17)) } }
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(17))
+        }
+    }
+}
 
-    tasks.withType<JavaCompile>().configureEach {
-        options.encoding = "UTF-8"
+subprojects {
+    tasks.withType<JavaCompile> {
+        options.encoding = Charsets.UTF_8.name()
         options.release.set(17)
+    }
+    tasks.withType<Javadoc> {
+        options.encoding = Charsets.UTF_8.name()
+    }
+    tasks.withType<ProcessResources> {
+        filteringCharset = Charsets.UTF_8.name()
     }
 
     repositories {
