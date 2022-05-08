5dc332ff11d4c58a82cf583729ff0f1d052ac34b
etil2jz <blanchot.arthur@protonmail.ch>
2022-05-08 13:23:01 +0200

    Update Upstream (Pufferfish)

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ repositories {
 dependencies {
     remapper("net.fabricmc:tiny-remapper:0.8.4:fat")
     decompiler("org.quiltmc:quiltflower:1.8.1")
-    paperclip("io.papermc:paperclip:3.0.2")
+    paperclip("io.papermc:paperclip:3.0.3-SNAPSHOT")
 }
 
 subprojects {
