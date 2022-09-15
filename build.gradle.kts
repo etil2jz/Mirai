0e66e7f59520c313591e9be82f9a638d26329e69
etil.sol <81570777+etil2jz@users.noreply.github.com>
2022-09-15 16:23:39 +0200

    [ci skip] Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     java
     `maven-publish`
     id("com.github.johnrengelman.shadow") version "7.1.2" apply false
-    id("io.papermc.paperweight.patcher") version "1.3.7-SNAPSHOT"
+    id("io.papermc.paperweight.patcher") version "1.3.9-SNAPSHOT"
 }
 
 repositories {
@@ -15,7 +15,7 @@ repositories {
 }
 
 dependencies {
-    remapper("net.fabricmc:tiny-remapper:0.8.4:fat")
+    remapper("net.fabricmc:tiny-remapper:0.8.6:fat")
     decompiler("org.quiltmc:quiltflower:1.8.1")
     paperclip("io.papermc:paperclip:3.0.3-SNAPSHOT")
 }
